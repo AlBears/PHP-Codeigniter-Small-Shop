d41552bd75c8784f569a61a58fc4ecac2ebeb683
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-08-16 20:21:10 +0300

    Products Model

---
+++ application/models/Product_model.php
@@ -0,0 +1,19 @@
+<?php
+
+class Product_model extends CI_Model {
+
+	public function get_products(){
+		$this->db->select('*');
+		$this->db->from('products');
+		$query = $this->db->get();
+		return $query->result();
+	}
+	//get single product
+	public function get_product_details($id){
+		$this->db->select('*');
+		$this->db->from('products');
+		$this->db->where('id', $id);
+		$query = $this->db->get();
+		return $query->row();
+	}
+}
--- application/views/details.php
+++ application/views/details.php
@@ -1,13 +1,17 @@
-<?php foreach($products as $product) : ?>
-	<div class="col-md-4 game">
-		<div class="game-price"><?php echo $product->price; ?></div>
-			<a href="<?php echo base_url();?>products/details/<?php echo $product->id; ?>">
-				<img src="<?php echo base_url();?>assets/images/products/<?php echo $product->image; ?>" />
-			</a>
-		<div class="game-title">
-			<?php echo $product->title; ?>
+<div class="row details">
+	<div class="col-md-4">
+		<img src="<?php echo base_url();?>assets/images/products/<?php echo $product->image; ?>" />
+	</div>
+	<div class="col-md-8">
+		<h3><?php echo $product->title; ?></h3>
+		<div class="details-price">
+			<?php echo $product->price; ?>
 		</div>
-		<div class="game-add">
+		<div class="details-description">
+			<p><?php echo $product->description; ?></p>
+		</div>
+		
+		<div class="details-buy">
 			<form method="post" action="<?php echo base_url(); ?>cart/add">
 				QTY: <input class="qty" type="text" name="qty" value="1" /><br>
 				<input type="hidden" name="item_number" value="<?php echo $product->id; ?>" />
@@ -17,4 +21,4 @@
 			</form>
 		</div>
 	</div>
-<?php endforeach; ?>
+</div>
