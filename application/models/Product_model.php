27e90ceb5a9fb446adc3caa7f6f3bc1228da2e33
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-08-17 19:34:56 +0300

    Sidebar

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -16,4 +16,21 @@ class Product_model extends CI_Model {
 		$query = $this->db->get();
 		return $query->row();
 	}
+	//get categories
+	public function get_categories(){
+		$this->db->select('*');
+		$this->db->from('categories');
+		$query = $this->db->get();
+		return $query->result();
+	}
+	//Most popular
+	public function get_popular(){
+		$this->db->select('P.*, COUNT(O.product_id) as total');
+		$this->db->from('orders AS O');
+		$this->db->join('products AS P', 'O.product_id = P.id', 'INNER');
+		$this->db->group_by('O.product_id');
+		$this->db->order_by('total', 'desc');
+		$query = $this->db->get();
+		return $query->result();
+	}
 }
